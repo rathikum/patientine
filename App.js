0e8dbafd1be10a7cdca41d20aa209b58041b490c
rathikum <47818365+rathishkumarcognizant@users.noreply.github.com>
2020-05-25 12:58:44 +0530

    code clean up

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import { StackNavigator } from "react-navigation";
-import FireBaseScreen from "./src/components/FireBase";
 import LoginScreen from "./src/components/Login/Login";
 import DashBoardScreen from "./src/components/DashBoard";
 import ProfileScreen from "./src/components/Profile";
-import PrimaryInsuranceScreen from "./src/components/PrimaryInsurance";
-import SecondaryInsuranceScreen from "./src/components/SecondaryInsurance";
 import RegistrationScreen from "./src/components/Registration";
 import ForgetPasswordScreen from "./src/components/ForgetPassword";
-import PatientProfileScreen from "./src/components/PatientProfile";
 import AppointmentScreen from "./src/components/Appointment";
-import ProcedureScreen from "./src/components/Procedure";
-import UpdatePrimaryInsuranceScreen from "./src/components/UpdatePrimaryInsurance";
-import UpdateSecondaryInsuranceScreen from "./src/components/UpdateSecondaryInsurance";
 import VisitNoteScreen from "./src/components/VisitNote";
-import VisitNoteDetailsScreen from "./src/components/VisitNoteDetails";
-import ActiveMedicationsScreen from "./src/components/ActiveMedications";
 import MedicationsScreen from "./src/components/Medications";
-import VideoAppointmentScreen from "./src/components/VideoAppointment";
-import DoctorListScreen from "./src/components/DoctorList";
-import VideoCallScreen from "./src/components/VideoCall";
 import PaymentScreen from "./src/components/Payment";
-import PreRegAppointment from "./src/components/PreRegAppointment";
 import Notification from "./src/components/Notification";
 import BillingScreen from "./src/components/Billing";
 import OnlinePayment from "./src/components/OnlinePayment";
@@ -33,25 +20,13 @@ import TermsCondition from "./src/components/TermsandCondition";
 
 const App = StackNavigator({
   Login: { screen: LoginScreen },
-  Procedure: { screen: ProcedureScreen },
   DashBoard: { screen: DashBoardScreen },
-  SecondaryInsurance: { screen: SecondaryInsuranceScreen },
-  PrimaryInsurance: { screen: PrimaryInsuranceScreen },
-  PatientProfile: { screen: PatientProfileScreen },
   VisitNote: { screen: VisitNoteScreen },
-  VideoCall: { screen: VideoCallScreen },
   Payment: { screen: PaymentScreen },
-  VisitNoteDetails: { screen: VisitNoteDetailsScreen },
-  UpdatePrimaryInsurance: { screen: UpdatePrimaryInsuranceScreen },
-  UpdateSecondaryInsurance: { screen: UpdateSecondaryInsuranceScreen },
-  DoctorList: { screen: DoctorListScreen },
   Medications: { screen: MedicationsScreen },
-  ActiveMedications: { screen: ActiveMedicationsScreen },
   Appointment: { screen: AppointmentScreen },
   Registration: { screen: RegistrationScreen },
   ForgetPassword: { screen: ForgetPasswordScreen },
-  VideoAppointment: { screen: VideoAppointmentScreen },
-  PreRegAppointment: { screen: PreRegAppointment },
   Notification: { screen: Notification },
   Billing: { screen: BillingScreen },
   Online : { screen : OnlinePayment },
